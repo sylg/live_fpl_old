7c3b6ee8fcb992a6c299d73c55e2ed5c34f6e11c
Sylvain Giuliani <sgiuliani@gmail.com>
2012-09-29 10:09:49 +0200

    celery on heroku fix

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from celery.schedules import crontab
+from tasks import redis_url 
 
 CELERY_TIMEZONE = 'Europe/London'
 
@@ -10,7 +11,6 @@ BROKER_URL = 'redis://'
 CELERY_IMPORTS = ("tasks", )
 
 ## Using the database to store task state and results.
-CELERY_RESULT_BACKEND = "redis://"
-
+CELERY_RESULT_BACKEND = redis_url
 
 
