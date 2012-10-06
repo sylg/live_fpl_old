98bcec79371dae92c1180f6e107f4115fada01b9
Sylvain Giuliani <sgiuliani@gmail.com>
2012-10-06 11:18:40 +0200

    deploy heroku fix3

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,18 +1,16 @@
 from datetime import timedelta
 from celery.schedules import crontab
+from push import *
 #from tasks import redis_url 
 
 CELERY_TIMEZONE = 'Europe/London'
 
 
-BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = redis_url #'redis://localhost:6379/0'
 
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ("tasks" )
 
 ## Using the database to store task state and results.
 #CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
-CELERY_REDIS_MAX_CONNECTIONS = 4
-BROKER_POOL_LIMIT = 4
 
