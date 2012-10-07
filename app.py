5431e4ddabdef284bbfa355e796006ee91733d41
Sylvain Giuliani <sgiuliani@gmail.com>
2012-10-07 23:34:15 +0200

    Working league scrapper & redis storing

--- app.py
+++ app.py
@@ -2,29 +2,60 @@ from flask import Flask, request, render_template
 import redis
 import os
 from push import *
+from collections import OrderedDict
+import urllib2
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 r.set('livefpl_status','')
-league = [[307, {'id': 37828, 'rank': 12, 'teamname':'PEDD United','gw_score':42 }],
-[356, {'id': 205633, 'rank': 6, 'teamname':'Fc Mbonabushia','gw_score':42 }],
-[312, {'id': 933573, 'rank': 11, 'teamname':'FC Slurp','gw_score':42 }],
-[362, {'id': 38861, 'rank': 5, 'teamname':'KFC_Overijse','gw_score':42 }],
-[396, {'id': 1538051, 'rank': 2, 'teamname':'Fc Paris','gw_score':42 }]]
-#league = {'PEDD United': {'id': 37828, 'rank': 12, 'totalpts': 307}, 'Fc Mbonabushia': {'id': 205633, 'rank': 6, 'totalpts': 356}, 'FC Slurp': {'id': 933573, 'rank': 11, 'totalpts': 312}, 'KFC_Overijse': {'id': 38861, 'rank': 5, 'totalpts': 362}, 'Fc Paris': {'id': 1538051, 'rank': 2, 'totalpts': 396}, 'CP Rangers': {'id': 194801, 'rank': 9, 'totalpts': 334}, 'FC Van Nico': {'id': 321564, 'rank': 7, 'totalpts': 342}, 'FC Lasne': {'id': 688922, 'rank': 1, 'totalpts': 407}, 'RSC Swarlz': {'id': 694831, 'rank': 4, 'totalpts': 383}, 'MonacoDiBaviera': {'id': 378429, 'rank': 3, 'totalpts': 388}, 'FC Jaboulani': {'id': 303108, 'rank': 8, 'totalpts': 337}, 'FC Gamos': {'id': 175286, 'rank': 10, 'totalpts': 317}}
-league = sorted(league, reverse=True)
+
+
+def getteams(leagueid):
+	url = 'http://fantasy.premierleague.com/my-leagues/%s/standings/' % leagueid
+	response = urllib2.urlopen(url)
+	html = response.read()
+	tablestart = html.find('<!-- League tables -->')
+	tableend = html.find('</section>')
+	html = html[tablestart:tableend]
+	soup = BeautifulSoup(html)
+	if len(soup.find_all('tr')) <= 25:
+		for team in soup.find_all('tr'):
+
+			if team.find('a') == None:
+				continue
+			teamname = str(team.find('a').string)
+			team_id = int(team.a['href'].strip('/').split('/')[1])
+			total_pts = int(team.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string)
+			gw_pts = int(team.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string)
+			r.sadd('league:%s'%leagueid, team_id)
+			for team in r.smembers('league:%s'%leagueid):
+				r.hmset('team:%s' %team_id,{'id':team_id, 'totalpts':total_pts, 'gwpts':gw_pts, 'teamname':teamname})
+	else:
+		r.sadd('league:%s'%leagueid,"toobig")
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
 	return render_template("index.html")
 
-
 @app.route("/live", methods=['GET', 'POST'])
 def live():
+	lid = str(request.args.get('league_id'))
+	getteams(lid)
+	league = []
+	if r.sismember('league:%s'%lid, "toobig") == 0:
+		for team in r.smembers('league:%s' % lid):
+			league.append(r.hgetall('team:%s'%str(team)))
+		league = sorted(league, key=lambda k: k['totalpts'],reverse=True)
+	else:
+		league.append('None')
 	return render_template("live.html",pushed_data=r.lrange('pushed_data',0,-1), league=league)
 
 @app.route("/status",methods=['GET','POST'])
 def status():
 	return r.get('livefpl_status')
+
 
 
 
--- testing.py
+++ testing.py
@@ -13,16 +13,28 @@ def getteams(leagueid):
 	tableend = html.find('</section>')
 	html = html[tablestart:tableend]
 	soup = BeautifulSoup(html)
-	league = []
+	if len(soup.find_all('tr')) <= 25:
+		print "its good"
+		for team in soup.find_all('tr'):
 
-	for team in soup.find_all('tr'):
-		if team.find('a') == None:
-			continue
-		teamnanme = str(team.find('a').string)
-		team_id = int(team.a['href'].strip('/').split('/')[1])
-		total_pts = int(team.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string)
-		league.append([total_pts,{'id':team_id, 'teamnanme':teamnanme}])
-
+			if team.find('a') == None:
+				continue
+			teamname = str(team.find('a').string)
+			team_id = int(team.a['href'].strip('/').split('/')[1])
+			total_pts = int(team.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string)
+			gw_pts = int(team.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string)
+			r.sadd('league:%s'%leagueid, team_id)
+			for team in r.smembers('league:%s'%leagueid):
+				r.hmset('team:%s' %team_id,{'id':team_id, 'totalpts':total_pts, 'gwpts':gw_pts, 'teamname':teamname})
+	else:
+		print "not good"
+		r.sadd('league:%s'%leagueid,"toobig")
 		
-	print league
-getteams(48483)
+
+getteams(29613)
+
+
+# for team in r.smembers('league:29613'):
+# 	r.delete('team:%s'%team)
+
+#league = sorted(league.items(), key= lambda x: x[1]['totalpts'], reverse=True)
