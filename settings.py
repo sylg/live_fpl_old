9c4abe869557294315d610284380563950fe1091
Sylvain Giuliani <sgiuliani@gmail.com>
2012-11-05 21:30:20 +0100

    lots of bug fix. trying to integrade player id

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ p = pusher.Pusher()
 
 #Ticker Channel
 
-ticker_channel = 'prod_ticker'
-# ticker_channel = 'dev_ticker'
+# ticker_channel = 'prod_ticker'
+ticker_channel = 'dev_ticker'
 
 
 
@@ -23,15 +23,15 @@ ticker_channel = 'prod_ticker'
 
 #Heroku
 
-redis_url =  os.getenv('OPENREDIS_URL', 'redis://localhost')
-r = redis.from_url(redis_url, db=0)
-rp = redis.from_url(redis_url, db=1)
+# redis_url =  os.getenv('OPENREDIS_URL', 'redis://localhost')
+# r = redis.from_url(redis_url, db=0)
+# rp = redis.from_url(redis_url, db=1)
 
 #Localhost
 
-# redis_url = 'redis://localhost:6379/0'
-# r = redis.StrictRedis(host='localhost', port=6379, db=0 )
-# rp = redis.StrictRedis(host='localhost', port=6379, db=1 )
+redis_url = 'redis://localhost:6379/0'
+r = redis.StrictRedis(host='localhost', port=6379, db=0 )
+rp = redis.StrictRedis(host='localhost', port=6379, db=1 )
 
 
 #Requests Headers
@@ -41,8 +41,8 @@ headers = {'User-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.go
 
 #Period Task timer
 
-timer = crontab(minute='*', hour='10-22',day_of_week='saturday,sunday,monday,tuesday,wednesday,thursday')
-#timer = timedelta(seconds=30)
+# timer = crontab(minute='*', hour='10-22',day_of_week='saturday,sunday,monday,tuesday,wednesday,thursday')
+timer = timedelta(seconds=30)
 
 
 
--- testing.py
+++ testing.py
@@ -9,91 +9,23 @@ import mechanize
 import re
 from settings import *
 
-def vc(teamid):
-	cap = r.hget('team:%s'%teamid, 'captain')
-	vc = r.hget('team:%s'%teamid, 'vc')
-	capmp = 0
-	vcmp = 0
-	capfid = 0
-	vcfid = 0
-	for fixture_id in r.lrange('fixture_ids',0,-1):
+rdb = redis.StrictRedis(host='localhost', port=6379, db=2)
 
-		if cap in r.lrange('lineups:%s'%fixture_id,0,-5):
-			capmp = r.hget('%s:old:%s'%(cap,fixture_id), 'MP')
-			capfid = fixture_id
-
-		if vc in r.lrange('lineups:%s'%fixture_id, 0, -5):
-			vcmp = r.hget('%s:old:%s'%(vc,fixture_id), 'MP')
-			vcfid = fixture_id
-
-	if capmp == 0 and vcmp > 0:
-		r.hincrby('team:%s'%team_id, 'cappts', r.hget('%s:old:%s'%(vc,vcfid), 'T')*2)
-	else:
-		r.hincrby('team:%s'%team_id, 'cappts', r.hget('%s:old:%s'%(cap,capfid), 'T')*2)
+def fill_playerdb():
+	i = 1
+	while i <= 20:
+		url = "http://fantasy.premierleague.com/web/api/elements/%s/" %i
+		print url
+		response = requests.get(url, headers=headers)
+		if response.status_code == 200:
+			json = response.json
+			web_name = json['web_name']
+			position = json['type_name']
+			teamname = json['team_name']
+	 		rdb.hmset(i,{'web_name':web_name, 'position':position,'teamname':teamname})
+	 	else:
+	 		print "got errore %s"%response.status_code
+		i += 1
 
 
-def reset():
-	for teamid in r.smembers('allteams'):
-		r.hset('team:%s'%teamid,'cappts',0)
-		print "changed cappts of %s"%teamid
-
-
-
-def update_gwpts(team):
-	print "adding gwpts for team %s ..."%team
-	for players in r.lrange('team:%s:lineup'%team,0, -5):
-		for ids in r.lrange('fixture_ids',0,-1):
-			if rp.exists('%s:old:%s'%(players,ids)):
-				print "%s is in the %s"%(players,ids)
-				if players != r.hget('team:%s'%team,'captain'):
-					print "%s is player with %s TP"%(players,rp.hget('%s:old:%s'%(players,ids), 'TP'))
-					r.hincrby('team:%s'%team, 'gwpts', rp.hget('%s:old:%s'%(players,ids), 'TP')) 
-				elif players == r.hget('team:%s'%team,'captain'):
-					print "%s is the captaion with %s TP"%(players, int(rp.hget('%s:old:%s'%(players,ids), 'TP'))*2)
-					r.hset('team:%s'%team, 'cappts', 0)
-					r.hincrby('team:%s'%team, 'cappts',  int(rp.hget('%s:old:%s'%(players,ids), 'TP'))*2)
-				r.hincrby('team:%s'%team, 'totalpts', r.hget('team:%s'%team, 'gwpts') )
-	print "Gwpts : (%s + %s ) pts & totalpts : %s pts"%(int(r.hget('team:%s'%team, 'gwpts')), int(r.hget('team:%s'%team, 'cappts')), r.hget('team:%s'%team, 'totalpts'))
-
-
-def livefpl_status():
-	url = "http://fantasy.premierleague.com/"
-	br = mechanize.Browser()
-	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-	br.open(url)
-	br.select_form(nr=0)
-	br.form['email'] = "baboo2@yopmail.com"
-	br.form['password'] = "bibi2000"
-	br.submit()
-	html = br.back().read()
-	
-	start = html.find('<table class="ismTable ismEventStatus ismHomeMarginTop">')
-	stop =  html.find('<div class="ismHomeMarginTop">')
-	html = html[start:stop]
-	soup = BeautifulSoup(html)
-	if "Live" in [str(td.string) for td in soup.find_all('td', {'class':'ismFinished'})]:
-		r.set('livefpl_status','Offline')
-	else:
-		r.set('livefpl_status','Live')
-
-	# soup.prettify()
-	
-
-def fplupdating():
-	url = 'http://fantasy.premierleague.com/fixtures/'
-	response = requests.get(url, headers=headers)
-	if len(response.history) != 0:
-		print "livefpl website is updating do nothing"
-		r.set('scrapmode', 'OFF')
-	else:
-		r.set('scrapmode', 'ON')
-		livefpl_status.delay()
-		getgw.delay()
-
-
-
-
-
-
-fplupdating()
-
+fill_playerdb()
